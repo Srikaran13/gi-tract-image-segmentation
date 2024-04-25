9afffe23b1572e90c48866d1bdc2f656590e1e64
Srikaran13 <srikaran1310@gmail.com>
2024-04-24 20:28:12 -0400

    Updated dataloader

--- src/data.py
+++ src/data.py
@@ -9,25 +9,14 @@ from pycocotools.coco import COCO
 from tqdm import tqdm
 import warnings
 warnings.simplefilter(action='ignore')
+
+from .utils.data_utils import get_scan_file_path, decode_rle, convert_binary_mask_to_rle
 # from ..config import BASE_PATH, CLASSES
 
 
-BASE_PATH = '//Users/srikaranreddy/Desktop/Spring Semester/Computer Vision 6.8300/cv-project/gi-tract-image-segmentation'
+BASE_PATH = '../../gi-tract-image-segmentation'
 CLASSES = ['small_bowel', 'large_bowel', 'stomach']
 IMAGE_SIZE = (128, 128)
-
-def get_scan_file_path(base_dir, scan_id):
-    """
-    Helper function to derive the 
-    full file path for a given scan ID
-    """
-    id_parts = scan_id.split("_")
-    case_part = id_parts[0]
-    day_part = "_".join(id_parts[:2])
-    scan_prefix = "_".join(id_parts[2:])
-    scan_directory = os.path.join(base_dir, case_part, day_part, "scans")
-    matching_files = glob(f"{scan_directory}/{scan_prefix}*")  # Expecting a single match
-    return matching_files[0]
 
 
 class SegmentationDataset():
@@ -81,72 +70,6 @@ class SegmentationDataset():
             })
         return images
 
-    def decode_rle(self, mask_rle, shape):
-        '''
-        Decode an RLE-encoded mask into a 2D numpy array.
-
-        Parameters:
-            mask_rle (str): Run-length encoding of the mask ('start length' pairs)
-            shape (tuple): The (height, width) dimensions of the output array
-
-        Returns:
-            numpy.ndarray: A 2D array where 1s represent the mask and 0s represent the background
-        '''
-        # Split the RLE string into a list of strings
-        encoded_pixels = mask_rle.split()
-        
-        # Extract start positions and lengths for the mask
-        starts = np.asarray(encoded_pixels[0::2], dtype=int) - 1  # Convert to 0-based indexing
-        lengths = np.asarray(encoded_pixels[1::2], dtype=int)
-        
-        # Calculate the end positions for each segment of the mask
-        ends = starts + lengths
-        
-        # Initialize a flat array for the image
-        flat_image = np.zeros(shape[0] * shape[1], dtype=np.uint8)
-        
-        # Apply the mask to the flat image array
-        for start, end in zip(starts, ends):
-            flat_image[start:end] = 1
-        
-        # Reshape the flat image array to 2D
-        return flat_image.reshape(shape)
-
-    def convert_binary_mask_to_rle(self, binary_mask):
-        '''
-        Convert a binary mask to run-length encoding.
-
-        Parameters:
-            binary_mask (numpy.ndarray): A 2D array where 1s represent the mask and 0s the background.
-
-        Returns:
-            dict: A dictionary with two keys: 'counts' and 'size', where 'counts' holds the RLE and 'size' the dimensions of the mask.
-        '''
-        # Initialize the RLE dictionary with the size of the mask
-        rle = {'counts': [], 'size': list(binary_mask.shape)}
-        
-        # Prepare to fill the counts list in the RLE dictionary
-        counts = rle['counts']
-        
-        # Flatten the array column-wise and group by value
-        flattened = binary_mask.ravel(order='F')
-        grouped = itertools.groupby(flattened)
-        
-        # Iterate through grouped data to form the RLE
-        for i, (pixel_value, group_iter) in enumerate(grouped):
-            # Convert group iterator to list to count elements
-            group_length = len(list(group_iter))
-            
-            # If first group is mask, prepend 0
-            if i == 0 and pixel_value == 1:
-                counts.append(0)
-            
-            # Append the length of each group to the RLE counts
-            counts.append(group_length)
-        
-        return rle
-
-    
     def create_annotations(self, df, images):
         annotations = []
         count = 0 
@@ -162,12 +85,12 @@ class SegmentationDataset():
             ids = df.index[df['id'] == file_id].tolist()
             if len(ids) > 0:
                 for idx in ids:
-                    segmentation_mask = self.decode_rle(
+                    segmentation_mask = decode_rle(
                         df.iloc[idx]['segmentation'], (height_slice, width_slice))
                     for contour in cv2.findContours(segmentation_mask, cv2.RETR_CCOMP, cv2.CHAIN_APPROX_NONE)[0]:
                         mask_image = np.zeros(segmentation_mask.shape, dtype=np.uint8)
                         cv2.drawContours(mask_image, [contour], -1, 255, -1)
-                        encoded_segmentation = self.convert_binary_mask_to_rle(mask_image)
+                        encoded_segmentation = convert_binary_mask_to_rle(mask_image)
                         ys, xs = np.where(mask_image)
                         x1, y1, x2, y2 = min(xs), min(ys), max(xs), max(ys)
                         annotations.append({
